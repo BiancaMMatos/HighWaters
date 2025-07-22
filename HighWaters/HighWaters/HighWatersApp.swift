a3f419529a658405f0422b46df3decad08877964
Bianca Maciel <macielmatosb@gmail.com>
2025-07-22 11:43:01 -0300

    Uploading XCode project

---
+++ HighWaters/HighWaters/HighWatersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighWatersApp.swift
+//  HighWaters
+//
+//  Created by Bianca Maciel on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HighWatersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HighWaters/HighWatersTests/HighWatersTests.swift
@@ -0,0 +1,17 @@
+//
+//  HighWatersTests.swift
+//  HighWatersTests
+//
+//  Created by Bianca Maciel on 22/07/25.
+//
+
+import Testing
+@testable import HighWaters
+
+struct HighWatersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

a32852aa195e0e7b5d992ef1f9d20038e807cc8b
Bianca Maciel <macielmatosb@gmail.com>
2025-07-27 19:26:24 -0300

    Implementing theview of the map with a button to click on

--- HighWaters/HighWaters/Map/View/MapView.swift
+++ HighWaters/HighWaters/Map/View/MapView.swift
@@ -1,24 +1,67 @@
 //
-//  ContentView.swift
+//  MapView.swift
 //  HighWaters
 //
 //  Created by Bianca Maciel on 22/07/25.
 //
 
+import MapKit
 import SwiftUI
+import CoreLocation
 
-struct ContentView: View {
+struct MapView: View {
+    
+    @StateObject private var locationViewModel = LocationViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            GeometryReader { reader in
+                Map(position: $locationViewModel.cameraPosition, interactionModes: .all) {
+                    UserAnnotation()
+                    
+                    ForEach(locationViewModel.annotations, id: \.self) {
+                        Marker("Flooded", coordinate: $0.coordinate)
+                    }
+                    
+                }
+                .mapControls {
+                    MapUserLocationButton()
+                }
+                
+                
+                Button {
+                    addFlood()
+                    
+                } label: {
+                    Text("Add flood")
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                .controlSize(.large)
+                .frame(width: 150)
+                .padding(.horizontal, (reader.size.width * 0.33))
+                .padding(.vertical, (reader.size.height * 0.85))
+                
+            }
+            
+            
         }
-        .padding()
     }
 }
 
+extension MapView {
+    
+    private func addFlood() {
+        do {
+            try locationViewModel.addFlood()
+        } catch {
+            print("Erro ao adicionar inundação")
+        }
+    }
+    
+}
+
 #Preview {
-    ContentView()
+    MapView()
 }

b32ca71fba128b5e30146058bfb5c639c75679ed
Bianca Maciel <macielmatosb@gmail.com>
2025-08-25 21:14:40 -0300

    Creating the user interface itself

--- HighWaters/HighWaters/Map/View/MapView.swift
+++ HighWaters/HighWaters/Map/View/MapView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct MapView: View {
+    
+    @StateObject private var viewModel = LocationViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomTrailing) {
+            LocationView(region: $viewModel.region,
+                         annotations: viewModel.annotations
+            )
+            .ignoresSafeArea(.all)
+            
+            Button(action: {
+                viewModel.centerToUser()
+            }, label: {
+                Image(systemName: "location.fill")
+                    .padding()
+                    .background(Circle().fill(Color.white))
+                    .shadow(radius: 3)
+            })
+            .padding()
+            
+        }
     }
 }
 

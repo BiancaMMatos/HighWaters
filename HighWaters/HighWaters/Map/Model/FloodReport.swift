bf6ad77445d3d68defd9cdac96ffd0cbb497e176
Bianca Maciel <macielmatosb@gmail.com>
2025-08-28 22:44:46 -0300

    Creating a structure for the flood report information

--- HighWaters/HighWaters/Map/Model/FloodReport.swift
+++ HighWaters/HighWaters/Map/Model/FloodReport.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct FloodReport: Codable {
+    var documentID: String?
+    let latitude: Double
+    let longitude: Double
+    var reportedDate: Date = Date()
+}
+
+
+extension FloodReport {
+    
+    init(latitude: Double, longitude: Double) {
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+    
+    func toDictionary() -> [String : Any?] {
+        return [
+            "documentID": self.documentID ?? nil,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+            "reportedDate": self.reportedDate.formatAsString()
+        ]
+    }
+}

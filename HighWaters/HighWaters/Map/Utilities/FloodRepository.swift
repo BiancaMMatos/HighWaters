4c21eb1ab1b1fc3960a23b87274c039e754b59a6
Bianca Maciel <macielmatosb@gmail.com>
2025-08-28 22:44:12 -0300

    Creating a repository for the flood

--- HighWaters/HighWaters/Map/Utilities/FloodRepository.swift
+++ HighWaters/HighWaters/Map/Utilities/FloodRepository.swift
@@ -5,4 +5,38 @@
 //  Created by Bianca Maciel on 27/08/25.
 //
 
-import Foundation
+import CoreLocation
+import FirebaseFirestore
+
+
+protocol FloodRepository {
+    func saveFlood(_ report: FloodReport)
+}
+
+
+final class FloodRepositoryImpl: FloodRepository {
+    
+    private lazy var db: Firestore = {
+        let firestoreDB = Firestore.firestore()
+        return firestoreDB
+    }()
+    
+    
+    func saveFlood(_ report: FloodReport) {
+        
+        var documentRef: DocumentReference? = nil
+        
+        documentRef = self.db.collection("flooded-regions").addDocument(data: report.toDictionary()) { error in
+            
+            if let error {
+                print("Error: \(error)")
+                
+            } else if let documentID = documentRef?.documentID {
+                var updatedReport = report
+                updatedReport.documentID = documentID
+            }
+        }
+    }
+    
+    
+}

d42b6a5268771d215caab04eb11c0f1d99d118b3
Bianca Maciel <macielmatosb@gmail.com>
2025-09-08 23:04:57 -0300

    Creating business logic on Service

--- HighWaters/HighWaters/Map/Utilities/FloodService.swift
+++ HighWaters/HighWaters/Map/Utilities/FloodService.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+protocol FloodServiceProtocol {
+    func reportFloods(completion: @escaping (Result<[FloodReport]?, Error>) -> Void) throws
+    func updateFloods(_ floods: [FloodReport])
+}
+
+
+final class FloodService: FloodServiceProtocol {
+    
+    let repository: FloodRepository
+    
+    init(repository: FloodRepository) {
+        self.repository = repository
+    }
+    
+    func reportFloods(completion: @escaping (Result<[FloodReport]?, any Error>) -> Void) throws {
+        do {
+            try repository.fetchFloods { [weak self] result in
+                switch result {
+                case .success(let floods):
+                    if let floods = floods {
+                        self?.updateFloods(floods)
+                    }
+                    completion(.success(floods))
+                    
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            throw FloodError.firebaseError(description: error.localizedDescription)
+        }
+    }
+    
+    func updateFloods(_ floods: [FloodReport]) {
+        DispatchQueue.main.async {
+            floods.forEach {
+                self.repository.saveNewFlood($0)
+            }
+        }
+    }
+
+}
+
+
+enum FloodError: Error, LocalizedError {
+    case firebaseError(description: String)
+    
+    var errorDescription: String? {
+        switch self {
+        case .firebaseError(let description):
+            "Firebase error: \(description)"
+        }
+    }
+}

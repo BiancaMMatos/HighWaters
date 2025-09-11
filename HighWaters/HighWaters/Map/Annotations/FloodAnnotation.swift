e408e43a55886340344c6e2f50be3f6c131fc635
Bianca Maciel <macielmatosb@gmail.com>
2025-09-11 13:48:20 -0300

    Creating structure to custom annotations

---
+++ HighWaters/HighWaters/Map/Annotations/FloodAnnotation.swift
@@ -0,0 +1,19 @@
+//
+//  FloodAnnotation.swift
+//  HighWaters
+//
+//  Created by Bianca Maciel on 09/09/25.
+//
+
+import MapKit
+import SwiftUI
+import Foundation
+
+
+final class FloodAnnotation: MKPointAnnotation {
+    let flood: FloodReport
+    
+    init(flood: FloodReport) {
+        self.flood = flood
+    }
+}
---
+++ HighWaters/HighWaters/Map/Model/FloodAnnotation.swift
@@ -0,0 +1,8 @@
+//
+//  AnnotationFlood.swift
+//  HighWaters
+//
+//  Created by Bianca Maciel on 09/09/25.
+//
+
+import Foundation

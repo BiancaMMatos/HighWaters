3bc11afa52b42d12dd81db1ff988b0b5bf2b96aa
Bianca Maciel <macielmatosb@gmail.com>
2025-09-11 21:11:44 -0300

    Creating a extension from buttons

--- HighWaters/HighWaters/Map/Model/Annotations/FloodAnnotation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FloodAnnotation.swift
-//  HighWaters
-//
-//  Created by Bianca Maciel on 09/09/25.
-//
-
-import Foundation
-
--- HighWaters/HighWaters/Map/Model/FloodAnnotation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AnnotationFlood.swift
-//  HighWaters
-//
-//  Created by Bianca Maciel on 09/09/25.
-//
-
-import Foundation
